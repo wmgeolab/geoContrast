d0ac850ab5b95a875ddc4317bccebc5c5c6a8ce1
Karim Bahgat <karim.bahgat.norway@gmail.com>
2021-09-27 12:04:25 +0200

    Rebuild Other, GRID3, SALB, WFP, and Natural Earth with quantized decimal precision

--- build.py
+++ build.py
@@ -8,7 +8,7 @@ import sys
 from datetime import datetime
 
 # params
-collections = ['OpenStreetMap']
+collections = ['Other','GRID3','SALB','WFP','Natural_Earth']
 isos = [] #['NOR','CHL','CAN','FRA','USA']
 replace = False
 write_meta = True
